aaf4d28ae8f018ba650a6e9612775f4c7285e411
Fadam40 <fadam40@gmail.com>
2025-01-06 16:23:58 +0100

    Update Destructor.sol

--- Destructor.sol
+++ Destructor.sol
@@ -1,8 +1,8 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-  contract Destructor {
-    function fallback() external{
+contract Destructor {
+    function destruct() external{
         selfdestruct(payable(msg.sender));
     }
 }
