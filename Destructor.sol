46b72fcfba4d8b4418d2f360fa9fcfc03dc3d992
Fadam40 <fadam40@gmail.com>
2025-01-06 16:07:21 +0100

    Update Destructor.sol

--- Destructor.sol
+++ Destructor.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
   contract Destructor {
-  function destruct() external{
-    selfdestruct(payable(msg.sender));
-  }
+    function fallback() external{
+        selfdestruct(payable(msg.sender));
+    }
 }
